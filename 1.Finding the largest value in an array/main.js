d49c6f1dcd29d36f04029420b70477002367b423
Andrew Lagutin <iamroypchel@gmail.com>
2022-05-16 20:38:03 +0300

    largest number in array script

---
+++ 1.Finding the largest value in an array/main.js
@@ -0,0 +1,13 @@
+//=== 1 option =============================================
+
+const array = [10, 4, 7, 1, 78, 45, 9];
+array.sort((a, b) => a - b); //sort from smallest to largest
+let maxNumberInArray = array[array.length - 1];
+
+//=== 2 option (faster) ====================================
+
+const secondArray = [10, 4, 7, 1, 78, 45, 9];
+let maxNum = 0;
+for (let i = 0; i < secondArray.length; i++) {
+  maxNum = Math.max(maxNum, secondArray[i]); //looping through the array, each larger value is assigned to a variable, so we find the largest number
+}
