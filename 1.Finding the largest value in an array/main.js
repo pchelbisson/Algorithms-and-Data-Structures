eddea0531fbbf40764a0bb6ad7121b33ed985375
Andrew Lagutin <iamroypchel@gmail.com>
2022-05-16 21:18:52 +0300

    add more option

--- 1.Finding the largest value in an array/main.js
+++ 1.Finding the largest value in an array/main.js
@@ -4,6 +4,11 @@ const array = [10, 4, 7, 1, 78, 45, 9];
 array.sort((a, b) => a - b); //sort from smallest to largest
 let maxNumberInArray = array[array.length - 1];
 
+//=== 1A option =============================================
+
+const arrray = [10, 4, 7, 1, 78, 45, 9];
+let max = Math.max(...array);
+
 //=== 2 option (faster) ====================================
 
 const secondArray = [10, 4, 7, 1, 78, 45, 9];
